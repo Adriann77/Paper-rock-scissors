01735e6cabcd2d6219ac6939de2fbaa902be6633
Adrian <klimasa98@gmail.com>
2023-09-23 21:14:49 -0400

    noob version finished rofl

--- js/script.js
+++ js/script.js
@@ -1,33 +1,66 @@
-const btns = document.querySelectorAll('button')
+const btns = document.querySelectorAll('.one')
 const PRS = ['paper', 'rock', 'scissors']
-
-const showPickedBtn = e => {
-	let choosedBtn = e.target.closest('button')
-	console.log(choosedBtn.classList.value)
-}
+const title = document.querySelector('h1')
+const playerPick = document.querySelector('.player-pick--btn')
+const AIPick = document.querySelector('.AI-pick')
+const result = document.querySelector('.result')
+const secondScreen = document.querySelector('.second-screen')
 
 const showResults = e => {
-	let playerPicked = e.target.closest('button').classList.value
-    let AIPicked = PRS[Math.floor(Math.random()*3)]
-    if( playerPicked === 'paper' && AIPicked === 'paper'){
-        console.log('its a tie');
-    }else if (playerPicked === 'paper' && AIPicked === 'rock'){
-        console.log('player wins');
-    }else if( playerPicked === 'paper' && AIPicked === 'scissors'){
-        console.log('AI wins');
-    }else if (playerPicked === 'rock' && AIPicked === 'paper'){
-        console.log('AI wins');
-    }else if(playerPicked === 'rock' && AIPicked === 'rock'){
-        console.log('its a tie');
-    }else if (playerPicked === 'rock' && AIPicked === 'scissors'){
-        console.log('player wins');
-    }else if (playerPicked === 'scissors' && AIPicked ==='paper'){
-        console.log('player wins');
-    }else if (playerPicked === 'scissors' && AIPicked ==='rock'){
-        console.log('AI wins');
-    }else if (playerPicked === 'scissors' && AIPicked ==='scissors'){
-        console.log('its a tie');
-    }
+	btns.forEach(el => el.classList.add('hide'))
+	title.classList.add('hide')
+	secondScreen.classList.add('show')
 
+	let playerPicked = e.target.closest('button').classList.value
+	let AIPicked = PRS[Math.floor(Math.random() * 3)]
+
+	console.log(playerPicked)
+	console.log(AIPicked)
+
+	if (playerPicked.includes('paper') && AIPicked === 'paper') {
+		result.textContent = 'Its a Tie'
+        playerPick.innerHTML = '<i class="fa-regular fa-hand"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand"></i></button>'
+	}
+    else if( playerPicked.includes('paper') && AIPicked === 'rock'){
+        result.textContent = 'You won'
+        playerPick.innerHTML = '<i class="fa-regular fa-hand"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand"></i></button>'
+    }
+    else if( playerPicked.includes('paper') && AIPicked === 'scissors'){
+        result.textContent = 'AI wins'
+        playerPick.innerHTML = '<i class="fa-regular fa-hand"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand-scissors"></i></button>'
+    }
+    else if( playerPicked.includes('rock') && AIPicked === 'paper'){
+        result.textContent = 'AI wins'
+        playerPick.innerHTML = '<i class="fa-solid fa-hand-fist"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand"></i></button>'
+    }
+    else if( playerPicked.includes('rock') && AIPicked === 'rock'){
+        result.textContent = 'Its a Tie'
+        playerPick.innerHTML = '<i class="fa-solid fa-hand-fist"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-solid fa-hand-fist"></i></button>'
+    }
+    else if( playerPicked.includes('rock') && AIPicked === 'scissors'){
+        result.textContent = 'You won'
+        playerPick.innerHTML = '<i class="fa-solid fa-hand-fist"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand-scissors"></i></button>'
+    }
+    else if( playerPicked.includes('scissors') && AIPicked === 'paper'){
+        result.textContent = 'You won'
+        playerPick.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand"></i></button>'
+    }
+    else if( playerPicked.includes('scissors') && AIPicked === 'rock'){
+        result.textContent = 'AI Wins'
+        playerPick.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-solid fa-hand-fist"></i></button>'
+    }
+    else if( playerPicked.includes('scissors') && AIPicked === 'scissors'){
+        result.textContent = 'Its a Tie'
+        playerPick.innerHTML = '<i class="fa-regular fa-hand-scissors"></i>'
+        AIPick.innerHTML = '<h2>AI pick:</h2><button><i class="fa-regular fa-hand-scissors"></i></button>'
+    }
 }
 btns.forEach(e => e.addEventListener('click', showResults))
